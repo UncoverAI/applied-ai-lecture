c3fb0c89d140ca43b2dd21650eb7ff37d84319bf
Christoph H <christophhoertnagl@gmail.com>
2024-12-08 15:03:32 +0100

    prepare training

--- VL6/lab/eval.py
+++ VL6/lab/eval.py
@@ -1,6 +1,13 @@
 from datasets import load_dataset
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
 from pydantic import BaseModel
+from torchmetrics.text import ROUGEScore
+
+ACTUAL_COL = "results"
+LABEL_COL = "label"
+CONVERSATION_COL = "conversation"
+ROUGE_COL = "rouge"
+CORRECT_COL = "correct"
 
 def chat(model, processor, conversation, image, max_new_tokens, verbose = True):
     # Preprocess the inputs
@@ -25,25 +32,47 @@ def chat(model, processor, conversation, image, max_new_tokens, verbose = True):
     return output_text[0]
 
 def make_conversation(sample):
-    sample["conversation"] = [
-        {"role": "user", "content": [{"type": "image"},{"type": "text",  "text": sample["query"]}]},
-        # {"role": "assistant", "content": [{"type": "text",  "text": sample["label"][0]}]}
+    sample[CONVERSATION_COL] = [
+        {"role": "user", "content": [{"type": "image"},{"type": "text",  "text": sample["query"]}]}
     ]
     return sample
 
+def save_dataset(ds, result_path: str):
+    # TODO potentially push to hub instead of using local storage
+    # Saving images is not necessary
+    ds = ds.remove_columns("image")
+    with open(result_path, "wb") as f:
+        ds.to_json(f)
+    
 def eval(model, processor, ds, max_new_tokens, result_path):
     model.eval()
     results = []
     for i, sample in enumerate(ds):
-        output = chat(model, processor, sample["conversation"], sample["image"], max_new_tokens=max_new_tokens, verbose=False)
+        output = chat(model, processor, sample[CONVERSATION_COL], sample["image"], max_new_tokens=max_new_tokens, verbose=False)
         results.append(output)
         print(f"Iteration {i}\n{output}\n\n{'#'*120}")
-    ds = ds.add_column("results", results)
-    ds = ds.remove_columns("image")
-    with open(result_path, "wb") as f:
-        ds.to_json(f)
+    ds = ds.add_column(ACTUAL_COL, results)
+    save_dataset(ds)
     return ds
 
+
+def compare(ds, actual_col: str = ACTUAL_COL, label_col: str = LABEL_COL):
+    rouge = ROUGEScore()
+    def row_compare(sample):
+        # we define "is_label_contained" as "correct" to have a simple binary measure
+        sample[CORRECT_COL] = sample[label_col][0] in sample[actual_col]
+        # we use rouge score to get a more nuanced measure
+        sample[ROUGE_COL] = rouge(preds=sample[actual_col], target=sample[label_col][0])['rouge1_fmeasure']
+        return sample
+    ds = ds.map(row_compare, num_proc=8)
+    save_dataset(ds)
+    return ds
+
+def compute_metrics(ds):
+    metrics = {}
+    metrics["accuracy"] = sum(ds["is_label_contained"]) / len(ds)
+    metrics["avg_rouge"] = sum(ds[ROUGE_COL]) / len(ds)
+    return metrics
 
 class EvalConfig(BaseModel):
     dataset: str
@@ -54,18 +83,26 @@ class EvalConfig(BaseModel):
     torch_dtype:str
     device_map:str
     max_new_tokens: int
-
+    adapter_path: str | None = None
+    
 
 def main(eval_config: EvalConfig | dict):
     eval_config = eval_config if isinstance(eval_config, EvalConfig) else EvalConfig(**eval_config)
     val_ds = load_dataset(eval_config.dataset, split=eval_config.split)
-    val_ds = val_ds.map(make_conversation, num_proc=eval_config.num_proc)
-
-    processor = AutoProcessor.from_pretrained(eval_config.model)
-    # Load the model in half-precision on the available device(s)
-    model = Qwen2VLForConditionalGeneration.from_pretrained(eval_config.model, torch_dtype=eval_config.torch_dtype, device_map=eval_config.device_map)
-    eval_ds = eval(model, processor , val_ds, eval_config.max_new_tokens, eval_config.output_path)
-    print(eval_ds)
+    if not CONVERSATION_COL in val_ds.column_names:
+        val_ds = val_ds.map(make_conversation, num_proc=eval_config.num_proc)
+    if not ACTUAL_COL in val_ds.column_names:
+        processor = AutoProcessor.from_pretrained(eval_config.model)
+        # Load the model in half-precision on the available device(s)
+        model = Qwen2VLForConditionalGeneration.from_pretrained(eval_config.model, torch_dtype=eval_config.torch_dtype, device_map=eval_config.device_map)
+        if eval_config.adapter_path:
+            model.load_adapter(eval_config.adapter_path)
+        eval_ds = eval(model, processor , val_ds, eval_config.max_new_tokens, eval_config.output_path)
+    if not (ROUGE_COL in val_ds.column_names and CORRECT_COL in val_ds.column_names):
+        eval_ds = compare(eval_ds)
+    eval_metrics = compute_metrics(eval_ds)
+    # TODO write metrics somewhere
+    print(eval_metrics)
 
 if __name__=="__main__":
     import argparse
---
+++ VL6/lab/train.py
@@ -0,0 +1,154 @@
+from transformers import BitsAndBytesConfig, Qwen2VLForConditionalGeneration, AutoProcessor, Qwen2VLProcessor
+import wandb
+from peft import LoraConfig, get_peft_model
+from trl import SFTConfig
+from pydantic import BaseModel
+from trl import SFTTrainer
+from datasets import load_dataset
+ 
+def make_conversation(sample):
+    sample["conversation"] = [
+        {"role": "user", "content": [{"type": "image"},{"type": "text",  "text": sample["query"]}]},
+        {"role": "assistant", "content": [{"type": "text",  "text": sample["label"][0]}]}
+    ]
+    return sample
+
+def get_collate_fn(processor):
+    # Create a data collator to encode text and image pairs
+    def collate_fn(examples):
+        # Get the texts and images, and apply the chat template
+        texts = [
+            processor.apply_chat_template(example["conversation"], tokenize=False) for example in examples
+        ]  # Prepare texts for processing
+        image_inputs = [example["image"] for example in examples]  # Process the images to extract inputs
+
+        # Tokenize the texts and process the images
+        batch = processor(
+            text=texts, images=image_inputs, return_tensors="pt", padding=True
+        )  # Encode texts and images into tensors
+
+        # The labels are the input_ids, and we mask the padding tokens in the loss computation
+        labels = batch["input_ids"].clone()  # Clone input IDs for labels
+        labels[labels == processor.tokenizer.pad_token_id] = -100  # Mask padding tokens in labels
+
+        # Ignore the image token index in the loss computation (model specific)
+        if isinstance(processor, Qwen2VLProcessor):  # Check if the processor is Qwen2VLProcessor
+            image_tokens = [151652, 151653, 151655]  # Specific image token IDs for Qwen2VLProcessor
+        else:
+            image_tokens = [processor.tokenizer.convert_tokens_to_ids(processor.image_token)]  # Convert image token to ID
+
+        # Mask image token IDs in the labels
+        for image_token_id in image_tokens:
+            labels[labels == image_token_id] = -100  # Mask image token IDs in labels
+
+        batch["labels"] = labels  # Add labels to the batch
+
+        return batch  # Return the prepared batch
+    return collate_fn
+
+class QuantConfig(BaseModel):
+    # BitsAndBytesConfig int-4 config
+    load_in_4bit: bool
+    bnb_4bit_use_double_quant: bool = True
+    bnb_4bit_quant_type: str = "nf4"
+
+class PeftConfig(BaseModel):
+    # Default Lora Config
+    lora_alpha: int # = 16
+    lora_dropout: float = 0.05
+    r: int = 8
+    bias: str = "none"
+    target_modules: list = ["q_proj", "v_proj"]
+    task_type: str = "CAUSAL_LM"
+
+class ModelConfig(BaseModel):
+    model_id: str
+    device_map: str
+    torch_dtype: str
+    max_tokens: int
+    # quant stuff
+    quant_config: QuantConfig | None = None
+    # lora stuff
+    peft_config: PeftConfig | None = None
+
+class Arguments(BaseModel):
+    output_dir: str
+    num_train_epochs: int
+    per_device_train_batch_size: int
+    per_device_eval_batch_size: int
+    gradient_accumulation_steps: int
+    optim: str
+    learning_rate: float
+    lr_scheduler_type: str
+    logging_steps: int
+    eval_steps: int
+    eval_strategy: str
+    save_strategy: str
+    bf16: bool
+    report_to: str = "wandb"
+    dataset_text_field: str = ""
+    remove_unused_columns: bool = False
+
+class TrainConfig(BaseModel):
+    dataset: str
+    num_proc: int
+    model: ModelConfig
+    args: Arguments
+
+def get_peft_model(model, peft_config: PeftConfig):
+    lora_config = LoraConfig(**peft_config.model_dump())
+    # Apply PEFT model adaptation
+    peft_model = get_peft_model(model, lora_config)
+    # Print trainable parameters
+    peft_model.print_trainable_parameters()
+
+def get_model(model_config: ModelConfig):
+    bnb_config = None
+    if model_config.quant_config:
+        bnb_config = BitsAndBytesConfig(**model_config.quant_config.model_dump(), bnb_4bit_compute_dtype=model_config.torch_dtype)
+    
+    # Load model and tokenizer
+    processor = Qwen2VLProcessor.from_pretrained(model_config.model_id)
+    model = Qwen2VLForConditionalGeneration.from_pretrained(
+        model_config.model_id, device_map=model_config.device_map, torch_dtype=model_config.torch_dtype, quantization_config=bnb_config
+    )
+    
+    if model_config.peft_config:
+        model = get_peft_model(model, model_config.peft_config)
+    return model, processor
+
+
+def main(train_config: TrainConfig | dict):
+    train_config = train_config if isinstance(train_config, TrainConfig) else TrainConfig(**train_config)
+    wandb.init(project="tune-qwen", config=train_config)
+    # Configure training arguments
+    training_args = SFTConfig(**train_config.args.model_dump())
+    model, processor = get_model(model_config=train_config.model)
+    
+    ds = load_dataset(train_config.dataset)
+    train_dataset, eval_dataset = ds["train"].map(make_conversation, num_proc=train_config.num_proc) , ds["test"].map(make_conversation, num_proc=train_config.num_proc)
+
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=get_collate_fn(processor),
+        peft_config=LoraConfig(**train_config.model.peft_config.model_dump()),
+        tokenizer=processor.tokenizer,
+    )
+
+    # trainer.train()
+    # trainer.save_model(training_args.output_dir)
+
+if __name__=="__main__":
+    import argparse
+    import yaml
+
+    parser = argparse.ArgumentParser(description='Run a Training')
+    parser.add_argument('--config', metavar='path', required=True, help='the path to config')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config_dict = yaml.safe_load(f)
+    main(config_dict)
