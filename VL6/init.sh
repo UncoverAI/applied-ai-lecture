8b81cf57f9247ee6863f6441d2f747933b35a2a6
UncoverAI <uncover.ai@bla.com>
2024-12-09 19:35:16 +0000

    add HF Token

--- VL6/init.sh
+++ VL6/init.sh
@@ -2,6 +2,7 @@
 
 echo "alias python='python3'" >> /home/ubuntu/.bashrc
 echo "export WANDB_API_KEY=<INSERT YOUR API KEY>" >> /home/ubuntu/.bashrc
+echo "export HF_TOKEN=<INSERT YOUR API TOKEN>" >> /home/ubuntu/.bashrc
 source /home/ubuntu/.bashrc
 
 git config --global user.name "UncoverAI" 
